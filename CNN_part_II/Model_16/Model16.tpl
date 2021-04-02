1fbab7088ba082d08891739a3ed17e11094e0efd
emanuelmfonseca <emanuelmfonseca@gmail.com>
2021-04-02 07:19:36 -0400

    Add files via upload

---
+++ CNN_part_II/Model_16/Model16.tpl
@@ -0,0 +1,44 @@
+//Number of population samples (demes)
+4
+//Population effective sizes (number of genes)
+NPOP1
+NPOP2
+NPOP3
+NPOPPLA
+//Sample sizes
+36
+36
+32
+18
+//Growth rates: negative growth implies population expansion
+0
+POP2Grow
+POP3Grow
+POPPLAGrow
+//Number of migration matrices : 0 implies no migration between demes
+2
+//Migration matrix 0
+0 0 0 0
+0 0 0 0
+0 0 0 0
+0 0 0 0
+//Migration matrix 1
+0 0 0 0
+0 0 MIG32 0
+0 MIG23 0 0
+0 0 0 0
+//historical event: time, source, sink, migrants, new size, new growth rate, migr. matrix
+7 historical event
+TSPLIT1 0 1 1 1 0 0
+TSPLIT2 2 3 1 1 0 0
+TSPLIT3 1 3 1 1 0 0
+TEXP2 2 2 0 RESIZE2 0 0
+TEXP3 1 1 0 RESIZE3 0 0
+TEXPPLA 3 3 0 RESIZEPLA 0 1
+TMIG 0 0 1 1 0 1
+//Number of independent loci [chromosome]
+500000
+//Per chromosome: Number of contiguous linkage Block: a block is a set of contiguous loci
+1
+//per Block:data type, number of loci, per gen recomb and mut rates
+DNA 1 0 1e-7
---
+++ CNN_part_II/Model_9/Model9.R
@@ -0,0 +1,102 @@
+library(parallel)
+
+CNN <- function(t){
+  
+  library(raster)
+  
+  ### Number on SNPs on the observed dataset
+  snps_observed <- 4009
+  
+  ### Number of sequences
+  n_seqs <- 122
+  
+  ### Order of the populations in the output
+  Pop1 <- 19:54
+  Pop3 <- 55:86
+  Pop2 <- 87:122
+  Planiceps <- 1:18
+  
+  setwd("~/Desktop/CNN_part_II/Model_9")
+  
+  tpl <- readLines("~/Desktop/CNN_part_II/Model_9/Model9.tpl")
+  
+  est <- readLines("~/Desktop/CNN_part_II/Model_9/Model9.est")
+  
+  setwd("~/Desktop/CNN_part_II/Model_9/Simulations")
+  
+  if(!dir.exists(paste0("sim",t))){
+    dir.create(paste0("sim",t))
+  }
+  
+  setwd(paste0("sim",t))
+  
+  write(tpl, "./Model9.tpl")
+  write(est, "./Model9.est")
+  
+  quiet <- function(x) {
+    sink(tempfile()) 
+    on.exit(sink()) 
+    invisible(force(x)) 
+  }
+  
+  path_to_fastsimcoal <- "~/Desktop/CNN_part_II/fsc26"
+  
+  ### Running fastsimcoal2
+  quiet(system(paste0(path_to_fastsimcoal, " -t ", paste0("~/Desktop/CNN_part_II/Model_9/Simulations/sim",t,"/Model9.tpl"), " -e ", paste0("~/Desktop/CNN_part_II/Model_9/Simulations/sim",t,"/Model9.est"), " -E 1 -n 1 -I -s 4009"), intern=T))
+  
+  setwd(paste0(getwd(), "/Model9"))
+  
+  out <- readLines("Model9_1_1.arp")
+  
+  lines <- which(grepl("^[[:digit:]]", out) == TRUE)
+  SNPs <- out[lines]
+  SNPs <- sub(".*\\s+(.*)", "\\1", SNPs)
+  SNPs <- strsplit(SNPs, "")
+  length.snps <- length(SNPs[[1]])
+  SNPs <- data.frame(matrix(unlist(SNPs), ncol = length.snps, nrow=n_seqs, byrow = T))
+  
+  SNPs <- SNPs[c(Pop1,Pop2,Pop3,Planiceps),]
+  
+  ### Converting SNPs to binary
+  if(!length.snps < snps_observed){
+    for (x in 1:length.snps){
+      len <- length(unique(SNPs[,x]))
+      count <- data.frame(matrix(NA,1,len))
+      for (i in 1:len){
+        count[1,i] <- length(which(SNPs[,x] == unique(SNPs[,x])[i]))
+        colnames(count)[i] <- unique(SNPs[,x])[i]
+      }
+      max <- colnames(count)[which(count == max(count))]
+      SNPs[,x] <- ifelse(as.numeric(SNPs[,x]) == as.numeric(max), 0, 1)   
+    }
+    
+    ### Sorting SNPs based on allele frequency
+    freq <- numeric()
+    for (x in 1:snps_observed){
+      freq <- c(freq, length(which(as.numeric(SNPs[,x])==1))/n_seqs)
+    }
+    
+    index <- sort(freq, index.return=TRUE, decreasing=T)
+    SNPs <- SNPs[,index$ix]
+    
+    ### Creating an image from the sorted allele matrix
+    r <- raster(nrow=n_seqs, ncol=snps_observed)
+    
+    seq <- c(t(SNPs))
+    r <- setValues(r,as.numeric(seq))
+    
+    png(filename=paste0("~/Desktop/CNN_part_II/Model_9/Model9_dataset/CNN_Model9_", t, ".png"),height=n_seqs, width=snps_observed)
+    dat <- matrix(getValues(r), ncol = n_seqs, nrow = snps_observed)
+    par(mai=c(0,0,0,0))
+    image(dat, col=c("black", "white"), axes=F, frame.plot=T)
+    dev.off()    
+    
+    setwd("~/Desktop/CNN_part_II/Model_9/Simulations")
+    unlink(paste0("sim",t), recursive=TRUE)
+    
+  } else {
+    setwd("~/Desktop/CNN_part_II/Model_9/Simulations")
+    unlink(paste0("sim",t), recursive=TRUE)
+  }}
+
+mclapply(1:15000, function(t) CNN(t), mc.cores=detectCores())
